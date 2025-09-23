059113e935a455ec2af65e1755b45ed5dc616bf3
Cuzo Miao <ascz123j@gmail.com>
2025-09-23 09:45:01 +0800

    init: eslint, prettier. vitest setting

--- README.md
+++ README.md
@@ -11,7 +11,6 @@ A modern, production-ready template for building full-stack React applications u
 - 📦 Asset bundling and optimization
 - 🔄 Data loading and mutations
 - 🔒 TypeScript by default
-- 🎉 TailwindCSS for styling
 - 📖 [React Router docs](https://reactrouter.com/)
 
 ## Getting Started
@@ -79,8 +78,7 @@ Make sure to deploy the output of `npm run build`
 ```
 
 ## Styling
-
-This template comes with [Tailwind CSS](https://tailwindcss.com/) already configured for a simple default starting experience. You can use whatever CSS framework you prefer.
+.
 
 ---
 
---
+++ eslint.config.js
@@ -0,0 +1,131 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import configPrettier from 'eslint-config-prettier';
+import { defineConfig } from 'eslint/config';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    }
+  },
+  {
+    files: ['app/**/*.{ts,mts,cts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        },
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json'
+      }
+    },
+    ignores: ['**/*.test.ts', '**/*.test.tsx', '**/*.spec.ts', '**/*.spec.tsx', 'app/test/**/*']
+  },
+  {
+    files: ['**/*.test.{ts,tsx}', '**/*.spec.{ts,tsx}', 'app/test/**/*.{ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        },
+        tsconfigRootDir: __dirname
+      }
+    }
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{ts,mts,cts,tsx}'],
+    rules: {
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'interface',
+          format: ['PascalCase'],
+          prefix: ['I'],
+        },
+        {
+          selector: 'typeAlias',
+          format: ['PascalCase'],
+          prefix: ['T'],
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          'argsIgnorePattern': '^_',
+          'varsIgnorePattern': '^_',
+          'caughtErrorsIgnorePattern': '^_',
+          'destructuredArrayIgnorePattern': '^_'
+        }
+      ],
+
+      ...configPrettier.rules, // 必須放在最後，以覆蓋其他規則
+    },
+  },
+  {
+    files: ['**/*.{jsx,tsx}'],
+    plugins: {
+      react: pluginReact,
+      'react-hooks': pluginReactHooks
+    },
+    settings: {
+      react: {
+        version: '18.3.1'
+      }
+    },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+
+      ...configPrettier.rules, // 必須放在最後，以覆蓋其他規則
+    }
+  },
+  {
+    ignores: [
+      '.react-router/**/*',
+      'node_modules/**/*',
+      'coverage/**/*',
+      'dist/**/*',
+      'build/**/*',
+      'public/**/*',
+      '**/*.d.ts',
+      '*.config.js',
+      '*.config.ts'
+    ]
+  }
+]);
